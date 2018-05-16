cf21b5fdea611e868fce30550d456aea6bb5e82f
whats0n <mrdvv@mail.ru>
2018-05-16 20:01:03 +0300

    update home

--- src/js/components/item.js
+++ src/js/components/item.js
@@ -1,16 +1,34 @@
 import {ACTIVE} from '../constants';
+import {TimelineMax} from 'gsap';
 
-const control = $('.js-nav-control');
 const containers = $('.js-item');
+const ANIMATED = 'is-animated';
 
 containers.each((i, container) => {
   container = $(container);
   const control = container.find('.js-item-control');
   const content = container.find('.js-item-content');
+  const slides = container.find('.js-item-slide');
+  const elements = container.find('.js-item-element');
+
+  const animation = new TimelineMax({ paused: true })
+    .addLabel('start')
+    .staggerTo(slides, 0.3, {
+      opacity: 1,
+      y: 0,
+      ease: Power1.easeOut
+    }, 0.2)
+    .eventCallback('onComplete', () => container.addClass(ANIMATED))
+    .eventCallback('onReverseComplete', () => container.addClass(ANIMATED));
 
   control.on('click', e => {
     e.preventDefault();
-    container.toggleClass(ACTIVE);
+    
     content.stop(true, true, true).slideToggle(400);
+    container.toggleClass(ACTIVE);
+
+    if (container.hasClass(ACTIVE)) {
+      animation.play(0);
+    }
   });
 });
